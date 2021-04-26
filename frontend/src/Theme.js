2212dda51c9d5bc890718bacba5c39b98b1a83da
MOBUMIN <kiju23@naver.com>
2021-04-27 08:15:41 +0900

    header 만듦
    * 배경색 적용
    * header 만듦
    * materialUI 용 palette 만듦

---
+++ frontend/src/Theme.js
@@ -0,0 +1,16 @@
+import { createMuiTheme } from '@material-ui/core';
+
+export default createMuiTheme({
+	palette:{
+		primary: {
+			main: '#FFFFFF'
+		},
+		secondary: {
+			main: '#000000'
+		},
+		text: {
+			primary: '#FFFFFF',
+			secondary: '#000000',
+		}
+	}
+});
