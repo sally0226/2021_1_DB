ed53ed5f2234a67c5e71c5b2a91f92f15b816d5f
MOBUMIN <kiju23@naver.com>
2021-05-18 18:04:31 +0900

    main 페이지 틀 제작

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Header } from '../components';
+import { Grid } from '@material-ui/core';
 
 function Main() {
+	const [movie, setMovie] = useState([1,2,3,4,5]);
 	return (
-		<div className="test">
+		<Grid className="main">
 			<Header />
-			<div className="testText">
-				Main
-			</div>
-		</div>
+			<Grid className="main-content">
+				<Grid className="slider">
+					영화 슬라이더 넣는 자리
+				</Grid>
+				<Grid className="movie-list">
+					{
+						movie.map((movie, i) => (
+							<Grid className="movies">
+								<span className="rank">{i}</span>
+								<span>영화사진</span>
+							</Grid>
+						))
+					}
+				</Grid>
+			</Grid>
+		</Grid>
 	)
 }
 
