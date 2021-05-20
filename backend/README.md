da2e9a216bd7e158f7b449921549d0dc95ebfc89
MOBUMIN <kiju23@naver.com>
2021-05-20 21:45:32 +0900

    Oracle 연동

--- backend/README.md
+++ backend/README.md
@@ -1,3 +1,5 @@
 # 백엔드 폴더
 
-스프링 초기 세팅하기
+node.js + express.js
+
+npm install 한 후 https://lts0606.tistory.com/183 대로 하기.
--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const cors = require('cors');
+const connect = require('./model/ConnectDB');
 const router = require('./routes/router');
-//const connOracleDB = require('./config/connOracleDB');
 
 app.use(cors());
 app.use(express.json());
 app.use('/',router);
 app.listen(port, () => console.log(`app listening on port ${port}!`))
 
-
+connect();
