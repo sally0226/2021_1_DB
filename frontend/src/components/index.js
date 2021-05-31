e00f8d72385adb2442c8cb3ac168e36b5735a2f0
MOBUMIN <kiju23@naver.com>
2021-05-31 13:52:29 +0900

    수정
    * modal 전체 쓸수있도록 긁어옴
    * 회원가입시 주민번호 입력하게 수정
    * 영화 호버시 예매하기 버튼 보이도록 수정

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Header } from './Header';
 export { default as Carousel } from './Carousel';
-export { default as PaginationDot } from './PaginationDot'
+export { default as PaginationDot } from './PaginationDot';
+export { default as Modal } from './Modal';
+export { default as CustomerInput } from './CustomerInput';
