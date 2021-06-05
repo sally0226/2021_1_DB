8287ed4e4797b40e14ad9b7ea19e1c804f7b40b9
sally0226 <sally734595@gmail.com>
2021-06-05 15:34:50 +0900

    관리자페이지 파일 이름 변경
    maintainer -> admin

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,8 +1,8 @@
 export { default as Main } from './Main';
 export { default as Login } from './Login';
 export { default as Register } from './Register';
-export { default as CreateMovie} from './Maintainer_createMovie';
+export { default as CreateMovie} from './Admin_createMovie';
 export { default as NowScreen } from './NowScreen';
 export { default as MovieDetail } from './MovieDetail';
 export { default as Reserve } from './Reserve';
-export { default as CreateEMP} from './Maintainer_createEMP';
+export { default as CreateEMP} from './Admin_createEMP';
