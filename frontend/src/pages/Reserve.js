66651bb24574dc1daa2e6807ed4f519dd2b45135
sally0226 <sally734595@gmail.com>
2021-06-09 01:08:59 +0900

    좌석 토글 기능 추가

--- frontend/src/pages/Reserve.js
+++ frontend/src/pages/Reserve.js
@@ -1,4 +1,4 @@
-import React, { forwardRef, useState } from 'react'
+import React, { forwardRef, useState, useEffect } from 'react'
 
 import { Grid, Button, FilledInput, FormControl, FormHelperText, InputAdornment, Link } from '@material-ui/core';
 import CreditCardIcon from '@material-ui/icons/CreditCard';
@@ -47,10 +47,12 @@ function Reserve() {
 };
 
 function StepContent ({step, next, prev, data, selectMovie, movieId}) {
-	if(step===0) return StepZero({next, data, selectMovie});
-	else if (step===1) return StepFirst({next, prev});
-	else if (step===2) return StepSecond({next, prev, data, movieId});
-	else return StepFinal({prev});
+
+	// if(step===0) return StepZero({next, data, selectMovie});
+	// else if (step===1) return StepFirst({next, prev});
+	// else if (step===2) return StepSecond({next, prev, data, movieId});
+	// else return StepFinal({prev});
+	return StepFirst({next, prev});
 }
 
 function StepZero({next, data, selectMovie}) {
@@ -132,17 +134,76 @@ function StepZero({next, data, selectMovie}) {
 	);
 };
 
+
 function StepFirst({next, prev}) {
+	
+	const seatRow = 10;
+	const seatCol = 20;
+	var seatsData = [[{
+		id: 'A1',
+		status: 0,
+		rating: 0
+	},{
+		id: 'A2',
+			status: 1,
+			rating: 0
+	},{
+		id: 'A3',
+			status: 0,
+			rating: 0
+	},{
+		id: 'A4',
+			status: 0,
+			rating: 0
+	},{
+		id: 'A5',
+			status: 0,
+			rating: 0
+	}],[{
+		id: 'B1',
+		status: 0,
+		rating: 1
+	},{
+		id: 'B2',
+		status: 0,
+		rating: 1
+	},{
+		id: 'B3',
+		status: 0,
+		rating: 1
+	},{
+		id: 'B4',
+		status: 0,
+		rating: 1
+	},{
+		id: 'B5',
+		status: 0,
+		rating: 1
+	}]];
+	const arr = Array.from(Array(seatRow), () => Array(seatCol).fill(false))
+	const [selectInfo, setSelectInfo] = useState({"arr": arr});
+
+	// useEffect(() => {}, [selectInfo]);
+	console.log(selectInfo);
 	function handelClick(e){
-		console.log(e.target);
-		console.log("clicked")
+		const c = e.target.getAttribute('c');
+		const r = e.target.getAttribute('r');
+		console.log("clicked");
+		const newState = selectInfo.arr;
+		// console.log(r +" " +c);
+		// console.log(newState[0]);
+		newState[r][c] = !newState[r][c];
+		setSelectInfo({arr: newState});
+		console.log(selectInfo);
 		
 	}
 	function Seat(props) {
-		if (props.isSelected === 1) {
+		if (selectInfo.arr[props.r][props.c] == true) {
 			return (
 				<div className="selected-seat"
 					id={props.id}
+					r={props.r}
+					c={props.c}
 					onClick={handelClick}>
 				</div>
 			)
@@ -151,15 +212,15 @@ function StepFirst({next, prev}) {
 			return (
 				<div className="seat"
 					id={props.id}
+					r={props.r}
+					c={props.c}
 					onClick={handelClick}>
 				</div>
 			)
-		}
-		
-	};
-	const seatRow = 10;
-	const seatCol = 20;
-	var seatsData = [];
+		}	
+	}
+	
+	
 	return (
 		<Grid className="stepFirst">
 			<Grid className="label">
@@ -171,7 +232,7 @@ function StepFirst({next, prev}) {
 					{[...Array(seatRow)].map((r, indexR) => (
 						<Grid className="seat-row" id={indexR}>
 							{[...Array(seatCol)].map((c, indexC) => (
-								<Seat isSelected={0} id={indexR+'-'+indexC}/>
+								<Seat isSelected={0} r={indexR} c={indexC}/>
 							))}
 						</Grid>
 					))}
@@ -179,7 +240,7 @@ function StepFirst({next, prev}) {
 				</Grid>
 			</Grid>			
 		</Grid>
-	);
+	)
 };
 function StepSecond({next, prev, data, movieId}) {
 	const [point, setPoint] = useState(0);
