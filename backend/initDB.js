91fa4daddbddc786d0a093989dce0abf79c088b4
MOBUMIN <kiju23@naver.com>
2021-06-12 17:46:13 +0900

    데이터 삭제 sql문 추가
    * initDB에서 데이터삭제 sql문 추가
    * 예시 이미지 서복 포스터 추가

--- backend/initDB.js
+++ backend/initDB.js
@@ -6,8 +6,13 @@ async function initDB()  {
 	try {
 		console.log('Initializing database module');
 		await database.initialize();
+
+		// 데이터 삭제 sql
 		// await conn.simpleExecute(`DELETE MEM`);
-		// await conn.simpleExecute(`DELETE CS`)
+		// await conn.simpleExecute(`DELETE CS`);
+		// await conn.simpleExecute(`DELETE TRAILER_SHOT`);
+		// await conn.simpleExecute(`DELETE TRAILER_VIDEO`);
+		// await conn.simpleExecute(`DELETE MOVIE`);
 	} catch (err) {
 		console.error(err);
 		process.exit(1); // Non-zero failure code
